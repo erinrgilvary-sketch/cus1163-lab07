3d49ee87d1e81d3ff996369458e9739bebe12813
erinrgilvary-sketch <erinrgilvary@gmail.com>
2026-04-17 16:43:13 -0400

    Add files via upload
    Finished homework

---
+++ RoundRobinLab.java
@@ -0,0 +1,103 @@
+package lab7;
+
+import java.util.*; 
+
+public class RoundRobinLab {
+		
+		static class Process{
+			int id;
+			int arrivalTime;
+			int burstTime;
+			int remainingTime;
+			int completionTime;
+			int turnaroundTime;
+			int waitingTime;
+			
+			
+		public Process(int id, int arrivalTime, int burstTime) {
+			this.id = id;
+			this.arrivalTime = arrivalTime;
+			this.burstTime = burstTime;
+			this.remainingTime = remainingTime;
+		}
+		
+		}
+		
+		public static void scheduleRoundRobin(List<Process> processes, int timeQuantum) {
+			int currentTime = 0;
+			
+			ArrayList<Process> queue = new ArrayList<Process>();
+			for (int i = 0; i< processes.size(); i ++) {
+				queue.add(processes.get(i));
+				
+			}
+			
+			while(queue.size() > 0) {
+				
+				Process current = queue.remove(0);
+				
+				int exececuteTime=Math.min(timeQuantum,current.remainingTime);
+				currentTime+=exececuteTime;
+				
+				currentTime = currentTime + exececuteTime; 
+				
+				current.remainingTime = current.remainingTime - exececuteTime; 
+				
+				if(current.remainingTime > 0) {
+					queue.add(current);
+				} else {
+					current.completionTime = currentTime;
+				}
+			}
+			for(int i = 0; i < processes.size(); i ++) {
+				Process p = processes.get(i);
+				p.turnaroundTime = p.completionTime - p.arrivalTime;
+				p.waitingTime = p.turnaroundTime - p.burstTime;
+				
+			}
+		
+		}
+		
+		public static void calculateMetrics(List<Process> processes, int timeQuantum) {
+		        System.out.println("========================================");
+		        System.out.println("Round Robin Scheduling Simulator");
+		        System.out.println("========================================\n");
+		        System.out.println("Time Quantum: " + timeQuantum + "ms");
+		        System.out.println("----------------------------------------");
+		        System.out.println("Process | Arrival | Burst | Completion | Turnaround | Waiting");
+
+		        double totalTurnaround = 0;
+		        double totalWaiting = 0;
+		        
+		        for (Process p : processes) {
+		            System.out.printf("   %d    |    %d    |   %d   |     %d     |     %d     |    %d\n",
+		                    p.id, p.arrivalTime, p.burstTime, p.completionTime,
+		                    p.turnaroundTime, p.waitingTime);
+		            totalTurnaround += p.turnaroundTime;
+		            totalWaiting += p.waitingTime;
+		        }
+
+		        System.out.println();
+		        System.out.printf("Average Turnaround Time: %.2fms\n", totalTurnaround / processes.size());
+		        System.out.printf("Average Waiting Time: %.2fms\n", totalWaiting / processes.size());
+		        System.out.println("========================================\n\n");
+		    }
+		public static void main(String[] args) {
+	        List<Process> processes1 = new ArrayList<>();
+	        processes1.add(new Process(1, 0, 7));
+	        processes1.add(new Process(2, 0, 4));
+	        processes1.add(new Process(3, 0, 2));
+
+	        scheduleRoundRobin(processes1, 3);
+	        calculateMetrics(processes1, 3);
+
+	        List<Process> processes2 = new ArrayList<>();
+	        processes2.add(new Process(1, 0, 7));
+	        processes2.add(new Process(2, 0, 4));
+	        processes2.add(new Process(3, 0, 2));
+
+	        scheduleRoundRobin(processes2, 5);
+	        calculateMetrics(processes2, 5);
+	    }
+
+}
